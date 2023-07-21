2ffb2ac752fb659dd442ee87555a7ef12efdd1de
Yassin Elshaiatany <yassin.elshaiatany@protonmail.com>
2023-07-22 02:07:15 +0300

    [MOD] Modified Website Security

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
     return render(request, 'pages/index.html')
 
 def dashboard(request):
-    return render(request, 'pages/dashboard.html')
+    if request.user.is_authenticated:
+        return render(request, 'pages/dashboard.html')
+    else:
+        messages.error(request, 'User is not logged in')
+        return redirect('login')
